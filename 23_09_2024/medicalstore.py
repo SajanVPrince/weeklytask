efb32d8184dedf122033032652945aea5460c9d8
SajanVPrince <sajanvprince@gmail.com>
2024-09-25 07:36:32 +0530

    updated

--- 23_09_2024/medicalstore.py
+++ 23_09_2024/medicalstore.py
@@ -6,7 +6,7 @@ def register():
         a=usr[-1]['id']
         b=int(a[4:])
         b+=1
-        id='user'+ str(b)
+        id=a[:4]+ str(b)
     email=str(input('enter your email : '))
     f=0
     for i in usr:
@@ -42,13 +42,48 @@ def add_stf():
             id='staff1000'
     else:
         a=stf[-1]['id']
-        b=int(a[4:])
+        b=int(a[5:])
         b+=1
-        id='staff'+ str(b)
+        id= a[:5]+ str(b)
     name=str(input('enter staff name : '))
     salary=int(input('enter staff salary : '))
     password=id
-    stf.append({'id':id,'name':name,'salary':salary,'password':password})
+    stf.append({'id':id,'name':name,'salary':salary,'password':password,'phone':'','location':'','email':''})
+
+def salup():
+    id=input('enter staff id : ')
+    f=0
+    for i in stf:
+        if i['id']==id:
+            salary=int(input('enter updated slary : '))
+            i['salary']=salary
+            f=1
+    if f==0:
+        print('invalid id')
+
+def delstf():
+    id=input('enter staff id : ')
+    f=0
+    for i in stf:
+        if i['id']==id:
+            stf.remove(i)
+            f=1
+    if f==0:
+        print('invalid id')
+    
+def vusr():
+    print('user details')
+    print('{:<10},{:<10}{:<10},{:<10},{:<10}'.format('ID','NAME','EMAIL','PHONE','LOCATION'))
+    print('_'*50)
+    for i in usr:
+        print('{:<10},{:<10}{:<10},{:<10},{:<10}'.format(i['id'],i['name'],i['email'],i['phone'],i['location']))
+
+def vstf():
+    print('Staff details')
+    print('{:<10},{:<10}{:<10}{:<10},{:<10},{:<10}'.format('ID','NAME','SALARY','PHONE','LOCATION','EMAIL'))
+    print('_'*30)
+    for i in stf:
+        print('{:<10},{:<10}{:<10}'.format(i['id'],i['name'],i['salary']))
 
 
 
@@ -78,10 +113,30 @@ while True:
                         print('''
                         1.Add Staff
                         2.Update Staff salary
-                        3.delete Staff''')
+                        3.delete Staff
+                        4.View Satff
+                        5.exit''')
                         c1=int(input('enter your choice : '))
                         if c1==1:
                             add_stf()
+                        elif c1==2:
+                            salup()
+                        elif c1==3:
+                            delstf()
+                        elif c1==4:
+                            for i in stf:
+                                print(i)
+                        elif c1==5:
+                            break
+                        else:
+                            print('invalid Choice')
+                elif c==2:
+                    vusr()
+                elif c==3:
+                    for i in med:
+                        print(i)
+                elif c==4:
+                    vstf()
         elif f==2:
             print('user login')
         elif f==3:
